e32d214cd6ee1772fdd9128a90864ccd203b7c71
Jim <83525577+linjim2235@users.noreply.github.com>
2023-03-29 10:55:26 +0800

    Update LASER.v

--- 2023/LASER.v
+++ 2023/LASER.v
@@ -1,4 +1,28 @@
-wire [8:0] cmp;
-reg[3:0] bigger1, bigger2;
-reg[3:0] smaller1, smaller2;
-assign cmp =( (bigger1 - smaller1)*(bigger1 - smaller1) + (bigger2 - smaller2)*(bigger2 - smaller2)) <= 16;
+// tmp_max_cover
+always @(posedge CLK)
+begin
+    if(RST)
+        tmp_max_cover <= 6'd0;
+    else if(current_state == CAL_COVER_RATE ||current_state == CAL_UP || current_state == CAL_DOWN || current_state == CAL_LEFT || current_state == CAL_RIGHT)
+    begin
+        if(counter >1)
+        begin
+            if(mul1 <=16 || mul2 <= 16)
+                tmp_max_cover <= max_cover + 1;
+        end
+    end
+end
+// max_cover
+always @(posedge CLK)
+begin
+    if(RST)
+        tmp_max_cover <= 6'd0;
+    else if(current_state == CAL_UP || current_state == CAL_DOWN || current_state == CAL_LEFT || current_state == CAL_RIGHT)
+    begin
+        if(counter == 6'd40)
+        begin
+            if(tmp_max_cover >max_cover)
+                max_cover <= tmp_max_cover;
+        end
+    end
+end
